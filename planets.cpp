2961412848ab8c1d5b52e0ad56424b2a47a0b953
keshavrmenon <keshavrmenon@gmail.com>
2023-02-06 15:54:33 +0530

    First working toy

--- planets.cpp
+++ planets.cpp
@@ -2,9 +2,8 @@
 #include<cmath>
 #include<vector>
 #include "positions.h"
-double dist(positions<double> r1, positions<double> r2, int norm=2);
 int main(){
-  std::vector<positions<double>> p={position(1,0),position(0,1)};
-  std::cout<<dist(p[0],p[1])<<std::endl;
+  std::vector<positions<double>> p{positions(std::vector<double>{1,0}),positions(std::vector<double>{0,1})};
+  std::cout<<reldist(p[0],p[1])<<std::endl;
   return 0;
 }
--- positions.h
+++ positions.h
@@ -12,20 +12,25 @@ class positions{
   //coordinates in arbitrary dimensions
   std::vector<T> r;
   
-  int dim=size(r);
+  int dim;
   
  public:
   positions(){
     r=NULL;
+    dim=0;
   }
   positions(std::vector<T> x){
     r = x;
+    dim=size(r);
   }
-  friend double dist(positions<T> r1, positions<T> r2, int norm=2){
+  std::vector<T> GetPositions(){
+    return r;
+  }
+  friend double reldist(positions<T> r1, positions<T> r2, int norm=2){
     if(r1.dim != r2.dim)
       throw std::runtime_error("Math error: Different dimensions");
     double d=0;
-    for (int i=0; i<r1.dim; ++i){
+    for (int i=0; i<r1.dim; i++){
       d+=pow((r1.r[i]-r2.r[i]),norm);
     }
     return pow(d,1.0/norm);
